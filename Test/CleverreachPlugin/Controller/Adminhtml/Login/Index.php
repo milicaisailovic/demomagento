b018cec62ba1437b5b163e327394814fe693ef23
Milica Isailovic <milica.isailovic@logeecom.com>
2021-10-15 16:54:12 +0200

    Create landing page
    ISSUE: MEMILICA-12

--- Test/CleverreachPlugin/Controller/Adminhtml/Login/Index.php
+++ Test/CleverreachPlugin/Controller/Adminhtml/Login/Index.php
@@ -1,1 +1,49 @@
 <?php
+namespace Test\CleverreachPlugin\Controller\Adminhtml\Login;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
+
+/**
+ * Class Index
+ */
+class Index extends Action implements HttpGetActionInterface
+{
+    const MENU_ID = 'Test_CleverreachPlugin::cleverreach_landingpage';
+
+    /**
+     * @var PageFactory
+     */
+    protected PageFactory $resultPageFactory;
+
+    /**
+     * Index constructor.
+     *
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     *
+     * @return Page
+     */
+    public function execute() : Page
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu(static::MENU_ID);
+        $resultPage->getConfig()->getTitle()->prepend(__(''));
+
+        return $resultPage;
+    }
+}
--- Test/CleverreachPlugin/registration.php
+++ Test/CleverreachPlugin/registration.php
@@ -1,1 +1,6 @@
 <?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Test_CleverreachPlugin',
+    __DIR__
+);
